7ff03d7711a81532f3b571429ac1e6add7262391
Vinita pal <pvinitak001@gmail.com>
2023-10-04 21:36:01 +0530

    Fixed issue
    Imported Scanner to take user input and print the greeting containing name of the user.

--- tasks/java/easy/GreetingApp.java
+++ tasks/java/easy/GreetingApp.java
@@ -6,6 +6,6 @@ public class GreetingApp {
         // TODO: Implement a Simple Greeting Application
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
-        System.out.println("Hello "+name+". Welcome to HactoberFest 2023. Hope You are having happy time contributing.");
+        System.out.println("Hello "+name+". Welcome to HactoberFest 2023. Hope You are having happy time contributing");
     }
 }
